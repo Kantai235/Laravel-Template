d976438a4221779cb0198945d5d9bbd2a29ba14b
Kantai <kantai.developer@gmail.com>
2022-05-22 19:50:15 +0800

    整理 Provider 註冊

---
+++ app/Providers/HelperServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+
+class HelperServiceProvider extends ServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        $rdi = new RecursiveDirectoryIterator(app_path('Helpers' . DIRECTORY_SEPARATOR . 'Global'));
+        $it = new RecursiveIteratorIterator($rdi);
+
+        while ($it->valid()) {
+            if (
+                !$it->isDot() &&
+                $it->isFile() &&
+                $it->isReadable() &&
+                $it->current()->getExtension() === 'php' &&
+                strpos($it->current()->getFilename(), 'Helper')
+            ) {
+                require $it->key();
+            }
+
+            $it->next();
+        }
+    }
+}
---
+++ app/Providers/LocaleServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class LocaleServiceProvider extends ServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        setAllLocale(config('app.locale'));
+
+        $this->registerBladeExtensions();
+    }
+
+    /**
+     * Register the locale blade extensions.
+     *
+     * @return void
+     */
+    protected function registerBladeExtensions(): void
+    {
+        /*
+         * The block of code inside this directive indicates
+         * the chosen language requests RTL support.
+         */
+        Blade::if('langrtl', function () {
+            return session()->has('lang-rtl');
+        });
+    }
+}
