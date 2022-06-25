759dfde8e04e870322f7c7dc14404285b2c69f0c
Kantai <kantai.developer@gmail.com>
2022-06-26 01:55:46 +0800

    新增 Config 檔案

--- config/auth.php
+++ config/auth.php
@@ -62,7 +62,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => App\Domains\Auth\Models\User::class,
         ],
 
         // 'users' => [
