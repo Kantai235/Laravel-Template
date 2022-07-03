c814b905d2bbaa1c2f42c5ea85118c6bf125035c
hanshino <hanshino@github>
2022-07-03 08:17:46 +0000

    修正語言選擇列表過窄問題

--- resources/views/includes/partials/lang.blade.php
+++ resources/views/includes/partials/lang.blade.php
@@ -1,4 +1,4 @@
-<ul class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownLanguageLink">
+<ul class="dropdown-menu dropdown-menu-right overflow-auto" style="max-height: 200px;" aria-labelledby="navbarDropdownLanguageLink">
     @foreach(collect(config('template.locale.languages'))->sortBy('name') as $code => $details)
         @if($code !== app()->getLocale())
             <x-utils.link class="dropdown-item pt-1 pb-1" :href="route('locale.change', $code)" :text="__(getLocaleName($code))" />
