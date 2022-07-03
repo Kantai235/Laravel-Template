2703864563c3fe35208057ae806d4e5206cf014d
Kantai <kantai.developer@gmail.com>
2022-07-03 15:41:05 +0800

    修正 Alert close 讓其在後台也能發揮作用

--- resources/views/components/utils/alert.blade.php
+++ resources/views/components/utils/alert.blade.php
@@ -1,9 +1,9 @@
 @props(['dismissable' => true, 'type' => 'success', 'ariaLabel' => __('Close')])
 
-<div {{ $attributes->merge(['class' => 'alert alert-dismissible alert-'.$type]) }} role="alert">
+<div {{ $attributes->merge(['class' => 'alert alert-dismissible alert-' . $type]) }} role="alert">
     {{ $slot }}
 
     @if ($dismissable)
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="{{ $ariaLabel }}"></button>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" data-coreui-dismiss="alert" aria-label="{{ $ariaLabel }}"></button>
     @endif
 </div>
