a0ef33c697c8682fb35c95ed6b15618fffb40799
Kantai <kantai.developer@gmail.com>
2022-07-04 20:13:22 +0800

    Admin 使用者增加 Announcement 權限

--- database/seeders/Auth/UserRoleSeeder.php
+++ database/seeders/Auth/UserRoleSeeder.php
@@ -23,6 +23,7 @@ class UserRoleSeeder extends Seeder
         $this->disableForeignKeys();
 
         User::find(1)->assignRole(config('template.access.role.admin'));
+        User::find(1)->assignRole(config('template.access.role.announcement'));
 
         $this->enableForeignKeys();
     }
