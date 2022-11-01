27312fbfa0506322f387e5068957a57e408ba12b
乾太 ₍₍ ◝(･◡･)◟ ⁾⁾ <Kantai235@users.noreply.github.com>
2022-11-01 22:26:13 +0800

    新增 API 專用的 Request

---
+++ app/Http/Requests/Api/JsonRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class JsonRequest.
+ */
+class JsonRequest extends FormRequest
+{
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response($validator->errors(), Response::HTTP_UNPROCESSABLE_ENTITY));
+    }
+}
