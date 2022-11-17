86bec74a4aedd2549c319d6535ae9a62445a66ba
乾太 ₍₍ ◝(･◡･)◟ ⁾⁾ <Kantai235@users.noreply.github.com>
2022-11-17 23:55:39 +0800

    修改 Docs 的連結

--- resources/views/backend/includes/footer.blade.php
+++ resources/views/backend/includes/footer.blade.php
@@ -2,7 +2,7 @@
     <div>
         <strong>
             @lang('Copyright') &copy; {{ date('Y') }}
-            <x-utils.link href="http://laravel-template.com" target="_blank" :text="__(appName())" />
+            <x-utils.link href="https://github.com/Kantai235/Laravel-Template" target="_blank" :text="__(appName())" />
         </strong>
 
         @lang('All Rights Reserved')
@@ -10,7 +10,7 @@
 
     <div class="ms-auto">
         @lang('Powered by')
-        <x-utils.link href="http://laravel-template.com" target="_blank" :text="__(appName())" /> &
+        <x-utils.link href="https://github.com/Kantai235/Laravel-Template" target="_blank" :text="__(appName())" /> &
         <x-utils.link href="https://coreui.io" target="_blank" text="CoreUI UI Components" />
     </div><!--ms-auto-->
 </footer><!--footer-->
