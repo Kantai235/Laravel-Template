a807fa192361f981174d6d3341ed28f689fc15d3
乾太 ₍₍ ◝(･◡･)◟ ⁾⁾ <Kantai235@users.noreply.github.com>
2022-11-17 23:56:51 +0800

    修正公告系統 Ends at 時間顯示錯誤的問題

--- resources/views/backend/announcement/includes/ends.blade.php
+++ resources/views/backend/announcement/includes/ends.blade.php
@@ -1,8 +1,8 @@
 <div style="position: inherit;">
     <strong style="font-weight: 600; font-size: 16px; color: #597a96; display: inherit;">
-        {{ isset($announcement->starts_at) ? $announcement->starts_at->toDateString() : '-' }}
+        {{ isset($announcement->ends_at) ? $announcement->ends_at->toDateString() : '-' }}
     </strong>
     <span style="font-size: 12px; font-weight: 400; color: #aab8c2;">
-        {{ isset($announcement->starts_at) ? $announcement->starts_at->toTimeString() : '' }}
+        {{ isset($announcement->ends_at) ? $announcement->ends_at->toTimeString() : '' }}
     </span>
 </div>
