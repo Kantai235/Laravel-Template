0f151f5d4401de48ce329ef4f969cd29a577e4bf
乾太 ₍₍ ◝(･◡･)◟ ⁾⁾ <Kantai235@users.noreply.github.com>
2023-02-14 09:33:21 +0800

    Update UserCheck.php

--- app/Domains/Auth/Http/Middleware/UserCheck.php
+++ app/Domains/Auth/Http/Middleware/UserCheck.php
@@ -6,11 +6,12 @@ use App\Domains\Auth\Models\User;
 use Closure;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Redirector;
 
 class UserCheck
 {
-    public function handle(Request $request, Closure $next): Redirector|RedirectResponse
+    public function handle(Request $request, Closure $next): Redirector|RedirectResponse|Response
     {
         if ($request->user() && $request->user()->isType(User::TYPE_USER)) {
             return $next($request);
